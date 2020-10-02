4e758b8a54c6c6b2a40fbea4c06de3c99f624816
Bhavya kalra <kalrabhavya13@gmail.com>
2020-10-02 18:25:56 +0530

    new commit

--- src/Components/Blogfield.jsx
+++ src/Components/Blogfield.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Markdown from "react-textarea-markdown";
 import '../Styles/Blogfield.scss';
 
 export default function Blogfield() {
     return (
         <div className="Blogfield">
-            <form className="blogform" method="POST">
-            <input className="blog_title" type="text" placeholder="Enter your Blog Title"></input>
-            <textarea className="blog_field" placeholder="Enter your blog"></textarea>
-            <button className="blog_publish" type="submit">Publish</button>
-            </form>
+            <div className="blogform">
+                <input className="blog_title" type="text" placeholder="Enter your Blog Title"></input>
+                <Markdown textarea={true} customWidth={[50,50]}/>
+                <button className="blog_publish" type="submit">Publish</button>
+            </div>
         </div>
     )
 }
