f0eb799ec3b30887f8b9c9544d0020cd0ab36888
root <p2012agarwal@gmail.com>
2020-10-02 21:23:34 +0530

    added header and blog field

--- src/Components/Blogfield.jsx
+++ src/Components/Blogfield.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Markdown from "react-textarea-markdown";
 import '../Styles/Blogfield.scss';
+import MdEditor from 'react-markdown-editor-lite';
+import 'react-markdown-editor-lite/lib/index.css';
+import MarkdownIt from 'markdown-it';
 
 export default function Blogfield() {
+    const mdParser = new MarkdownIt(/* Markdown-it options */);
     return (
         <div className="Blogfield">
             <div className="blogform">
                 <input className="blog_title" type="text" placeholder="Enter your Blog Title"></input>
-                <Markdown textarea={true} customWidth={[50,50]}/>
-                <button className="blog_publish" type="submit">Publish</button>
+                <div className="blog_field">
+                    <MdEditor
+                    style={{ height: "500px" }}
+                    renderHTML={(text) => mdParser.render(text)}
+                    />
+                </div>
+                <button className="btn-publish btn btn-outline-primary" type="button">Publish</button>
             </div>
         </div>
     )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,67 @@
-import React from 'react';
-import '../Styles/header.scss';
+import React from "react";
+import "../Styles/header.scss";
 
 function Header() {
+  const data=[""];
+  const categories = data.map((d) => <li key={d.name}>{d.name}</li>);
+  function navbar() {
+
+  }
+
   return (
     <div className="Header">
-      <h1>Hello</h1>
+      <nav class="navbar">
+        <div class="container">
+          <div class="navbar-header">
+            <button class="navbar-toggler" onClick={navbar}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+            <a href="/#">
+              <h4>
+                Awesome<span>logo</span>
+              </h4>
+            </a>
+          </div>
 
-
+          <div class="navbar-menu" id="open-navbar1">
+            <ul class="navbar-nav">
+              <li class="active">
+                <a href="/#">Home</a>
+              </li>
+              <li class="navbar-dropdown">
+                <a
+                  href="/#"
+                  class="dropdown-toggler"
+                  data-dropdown="my-dropdown-id"
+                >
+                  Categories <i class="fa fa-angle-down"></i>
+                </a>
+                <ul class="dropdown" id="my-dropdown-id">
+                  {categories}
+                </ul>
+              </li>
+              <li class="navbar-dropdown">
+                <a href="/#" class="dropdown-toggler" data-dropdown="blog">
+                  Blog <i class="fa fa-angle-down"></i>
+                </a>
+                <ul class="dropdown" id="blog">
+                  <li>
+                    <a href="/#">View Your Blog</a>
+                  </li>
+                  <li>
+                    <a href="/#">Create New Blog</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="/#">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
